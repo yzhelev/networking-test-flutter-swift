ba40f239d3b3029aab97d706f128a09bfb448b0b
Atanas Chervarov <a.chervarov@bitpioneers.com>
2021-03-05 15:32:57 +0200

    - adding request

--- android/app/src/main/kotlin/com/bitioneers/networking_test/NetworkingApi.kt
+++ android/app/src/main/kotlin/com/bitioneers/networking_test/NetworkingApi.kt
@@ -1,20 +1,20 @@
 package com.bitioneers.networking_test
-
 import android.os.Handler
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.lang.Exception
+import java.lang.reflect.Type
 import java.net.HttpURLConnection
 import java.net.URL
 import java.util.concurrent.Executor
 import kotlin.collections.HashMap
-
 class NetworkingApi(
         private val gson: Gson,
         private val executor: Executor,
-        private val handler: Handler) {
-
+        private val handler: Handler
+) {
     /**
      * Make an asynchronous request towards a given url
      * @param method Request method
@@ -29,11 +29,15 @@ class NetworkingApi(
             parameters: HashMap<String, Any>?,
             headers: HashMap<String, String>?,
             completion: (Any?, String?) -> Unit
-    )  {
+    ) {
         executor.execute {
             try {
                 val result = syncRequest(method, urlString, parameters, headers)
-                handler.post { completion(result, null) }
+                if (result is String) {
+                    handler.post { completion(null, result) }
+                } else {
+                    handler.post { completion(result, null) }
+                }
             } catch (e: Exception) {
                 e.printStackTrace()
                 handler.post { completion(null, e.message) }
@@ -47,33 +51,28 @@ class NetworkingApi(
      * @param urlString url
      * @param parameters optional parameters to add to the request
      * @param headers optional headers to add to the request
-     * @return returns a string result
+     * @return returns a Any result
      */
     private fun syncRequest(
             method: String? = "GET",
             urlString: String,
             parameters: HashMap<String, Any>?,
-            headers: HashMap<String, String>?): String? {
-
+            headers: HashMap<String, String>?
+    ): Any? {
         val url = URL(urlString)
-        var result: String?  = null
-
+        var result: String? = null
+        var resultMap: HashMap<Any, Any>? = null
         //Create connection request
         (url.openConnection() as? HttpURLConnection)?.run {
-
             //setup request method type
             requestMethod = method
             doOutput = true
-
             //setup headers if any
             headers?.forEach { addRequestProperty(it.key, it.value) }
-
             //parse parameters if any
             val jsonData = parameters?.let { gson.toJson(it) }
-
             //write data
             jsonData?.let { outputStream.write(it.toByteArray()) }
-
             //read response
             val reader = BufferedReader(InputStreamReader(inputStream))
             var line: String? = reader.readLine()
@@ -82,9 +81,16 @@ class NetworkingApi(
                 result += line;
                 line = reader.readLine();
             }
+            if (result != null) {
+                try {
+                    val type: Type = object : TypeToken<HashMap<Any?, Any?>?>() {}.type
+                    resultMap = gson.fromJson<HashMap<Any, Any>>(result, type)
+                } catch (e: Exception) {
+                    return e.message
+                }
+            }
             inputStream.close()
         }
-
-        return result
+        return resultMap
     }
 }
